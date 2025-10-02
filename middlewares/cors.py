0cffbd4bd93a57a50533ce607abc01a84fc440e6
FelipeRos19 <feliperos.simao@gmail.com>
2025-10-02 20:20:56 -0300

    fix cors

--- middlewares/cors.py
+++ middlewares/cors.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 def setup_cors(app: FastAPI):
     origins = [
-        "https://emails.felipe.fun"
+        "https://emails.felipe.fun/"
     ]
 
     app.add_middleware(
