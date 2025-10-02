59567e739f925a78f5afb263ac18b71a47a9228d
FelipeRos19 <feliperos.simao@gmail.com>
2025-10-02 19:58:29 -0300

    fix cors

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from routes.api_routes import api_router
 
 app = FastAPI()
 
-rate_limiter = RateLimiter(int(os.getenv("RATE_LIMIT")), int(os.getenv("TIME_WINDOW")))
 setup_cors(app)
+rate_limiter = RateLimiter(int(os.getenv("RATE_LIMIT")), int(os.getenv("TIME_WINDOW")))
 
 app.middleware("http")(rate_limiter)
 app.include_router(api_router)
