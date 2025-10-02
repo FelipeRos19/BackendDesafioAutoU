840c23e594f57cef6605400cf0936d2b80780aec
FelipeRos19 <feliperos.simao@gmail.com>
2025-10-02 20:10:47 -0300

    fix cors

--- middlewares/cors.py
+++ middlewares/cors.py
@@ -1,7 +1,7 @@
 import os
 
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 def setup_cors(app: FastAPI):
     origins = [
@@ -13,5 +13,5 @@ def setup_cors(app: FastAPI):
         allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],
-        allow_headers=["*"],
+        allow_headers=["Content-Type"],
     )
