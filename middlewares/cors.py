b548f35b3e9b0cda74f5b76ee7e8dc939581432a
FelipeRos19 <feliperos.simao@gmail.com>
2025-10-02 20:13:39 -0300

    fix cors

--- middlewares/cors.py
+++ middlewares/cors.py
@@ -12,6 +12,6 @@ def setup_cors(app: FastAPI):
         CORSMiddleware,
         allow_origins=origins,
         allow_credentials=True,
-        allow_methods=["*"],
+        allow_methods=['GET', 'POST', 'OPTIONS'],
         allow_headers=["Content-Type"],
     )
