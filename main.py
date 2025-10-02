beafa370cd59c120f6ae2de57e296585456acae5
FelipeRos19 <feliperos.simao@gmail.com>
2025-10-02 20:34:20 -0300

    fix cors

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import os
 
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 
-from middlewares.cors import setup_cors
 from middlewares.rate_limiter import RateLimiter
 from routes.api_routes import api_router
 
 app = FastAPI()
 
-setup_cors(app)
+origins = [
+    "https://emails.felipe.fun",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+)
+
 rate_limiter = RateLimiter(int(os.getenv("RATE_LIMIT")), int(os.getenv("TIME_WINDOW")))
 
 app.middleware("http")(rate_limiter)
--- middlewares/cors.py
+++ middlewares/cors.py
@@ -1,16 +1,15 @@
-import os
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 def setup_cors(app: FastAPI):
     origins = [
-        "https://emails.felipe.fun/"
+        "https://emails.felipe.fun"
     ]
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
