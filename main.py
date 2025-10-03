21d08226a25eb65d38001739cd187b84a8f50837
FelipeRos19 <feliperos.simao@gmail.com>
2025-10-02 22:20:30 -0300

    fix cors

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from middlewares.rate_limiter import RateLimiter
 from routes.api_routes import api_router
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
@@ -19,6 +20,14 @@ app.add_middleware(
     allow_headers=["*"],
     allow_credentials=False
 )
+
+@app.middleware("http")
+async def https_redirect(request: Request, call_next):
+    if request.headers.get("x-forwarded-proto") == "http":
+        url = request.url.replace(scheme="https")
+        return RedirectResponse(url)
+    response = await call_next(request)
+    return response
 
 #rate_limiter = RateLimiter(int(os.getenv("RATE_LIMIT")), int(os.getenv("TIME_WINDOW")))
 
